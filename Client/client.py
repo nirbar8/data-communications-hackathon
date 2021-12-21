9a63390550838340891eab3828e767a50ef09e41
omerpresler <41909419+omerpresler@users.noreply.github.com>
2021-12-21 20:04:01 +0200

    UDP connection acceptance

--- Client/client.py
+++ Client/client.py
@@ -1,7 +1,29 @@
+import socket
+
+bufferSize = 1024
+udpPort = 13117
+
+"""
+Listen for udp broadcasts and return the required information for the TCP connections
+"""
+
+
+def getIpAndPort():
+    while True:
+        UDPSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
+        UDPSocket.bind(('', udpPort))
+        msgFromServer = UDPSocket.recvfrom(bufferSize)
+        serverIp = msgFromServer[1]
+        msgFromServer = msgFromServer[0]
+        if len(msgFromServer) >= 7:
+            if msgFromServer[0:5] == 0xabcddcba:
+                if msgFromServer[5] == 2:
+                    return serverIp, msgFromServer[6:8]
+
+
 def main():
-    print("Hello World! From Client")
+    getIpAndPort()
 
 
 if __name__ == "__main__":
     main()
-

c63151a113bfe4182528f5863ac477e70b734493
Nir Barel <banir@post.bgu.ac.il>
2021-12-21 19:25:57 +0200

    hello world skeleton files

---
+++ Client/client.py
@@ -0,0 +1,7 @@
+def main():
+    print("Hello World! From Client")
+
+
+if __name__ == "__main__":
+    main()
+
---
+++ Server/server.py
@@ -0,0 +1,7 @@
+def main():
+    print("Hello World! From Server")
+
+
+if __name__ == "__main__":
+    main()
+

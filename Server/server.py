eb675629036fb4e980f03f7b611475bde919d450
Nir Barel <banir@post.bgu.ac.il>
2021-12-21 21:07:49 +0200

    listening tcp port, problem with permissions

--- Server/server.py
+++ Server/server.py
@@ -1,5 +1,7 @@
 from scapy.all import *
 from time import sleep
+import socket
+import os
 
 TESTING_MODE = False                # For Testing, switch to True
 
@@ -10,14 +12,14 @@ UDP_PORT = 13117
 def get_src_ip():
     network_name = 'eth2' if TESTING_MODE else 'eth2'   
     if network_name in get_if_list():
-        return 'blablabla' # get_if_addr(network_name)        #TODO: remove comment when testing
+        return get_if_addr(network_name)        #TODO: remove comment when testing
     else:
         raise RuntimeError(f"Network {network_name} not exist.")
         
 
 
 def send_game_offer(port):
-    packet = "\xab\xcd\xdc\xba" + "\x02" + port 
+    packet = "\xab\xcd\xdc\xba" + "\x02" + str(chr(port)) 
     send(IP(dst="255.255.255.255")/UDP(dport=UDP_PORT)/Raw(load=packet))
 
 
@@ -28,7 +30,8 @@ def main():
             print(f'Server started, listening on IP address {src_ip}')
             while True:
                 serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-                serversocket.bind((serversocket.gethostname(), 0))
+                serversocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+                serversocket.bind((os.uname()[1], 0))
                 _, src_port = serversocket.getsockname()
                 serversocket.listen(MAX_CLIENTS)
                 send_game_offer(src_port)
